352466a176225aad5fa7f2dc017d1fe490267c03
Harutyun <91012818+HarutyunTovmasyan19994@users.noreply.github.com>
2023-03-09 12:21:09 +0400

    tictak

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-   
+      <div className='tictac'>
+        <div className='tictac1'>
+          <div className='tictak2'></div>
+          <div className='tictak2'></div>
+          <div className='tictak2'></div>
+        </div>
+        <div className='tictac1'>
+        <div className='tictak2'></div>
+          <div className='tictak2'></div>
+          <div className='tictak2'></div>
+        </div>
+        <div className='tictac1'>
+        <div className='tictak2'></div>
+          <div className='tictak2'></div>
+          <div className='tictak2'></div>      
+            </div>
+      </div>
     </div>
   );
 }
